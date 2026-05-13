95e62a571f5909c696dda0a1414b2f9cb56598bf
paofuxiaomiao <1404441791@qq.com>
2026-05-13 15:27:35 +0800

    Configure app base path for GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Router, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { ProgressProvider } from "./contexts/ProgressContext";
@@ -12,18 +12,22 @@ import Level2 from "./pages/Level2";
 import Level3 from "./pages/Level3";
 import Ending from "./pages/Ending";
 
-function Router() {
+const routerBase = import.meta.env.BASE_URL.replace(/\/$/, "");
+
+function AppRouter() {
   return (
-    <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/explore"} component={Explore} />
-      <Route path={"/level1"} component={Level1} />
-      <Route path={"/level2"} component={Level2} />
-      <Route path={"/level3"} component={Level3} />
-      <Route path={"/ending"} component={Ending} />
-      <Route path={"/404"} component={NotFound} />
-      <Route component={NotFound} />
-    </Switch>
+    <Router base={routerBase}>
+      <Switch>
+        <Route path={"/"} component={Home} />
+        <Route path={"/explore"} component={Explore} />
+        <Route path={"/level1"} component={Level1} />
+        <Route path={"/level2"} component={Level2} />
+        <Route path={"/level3"} component={Level3} />
+        <Route path={"/ending"} component={Ending} />
+        <Route path={"/404"} component={NotFound} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
@@ -34,7 +38,7 @@ function App() {
         <ProgressProvider>
           <TooltipProvider>
             <Toaster />
-            <Router />
+            <AppRouter />
           </TooltipProvider>
         </ProgressProvider>
       </ThemeProvider>
